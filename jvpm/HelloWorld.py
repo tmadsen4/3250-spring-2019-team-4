e00cd20df0f3b65ffb89de2dea937a374fe8ad19
snubeagle <rmccull7@msudenver.edu>
2019-01-28 09:32:46 -0700

    added Ryan to outputs and test cases

--- jvpm/HelloWorld.py
+++ jvpm/HelloWorld.py
@@ -1,6 +1,7 @@
 def HelloWorld():
     print("Hello world")
     print("Hello Kenji Morizono")
+	print("Hello Ryan McCullough")
 
 if '__main__' == __name__:
     HelloWorld()
--- jvpm/test/test_HelloWorld.py
+++ jvpm/test/test_HelloWorld.py
@@ -10,4 +10,5 @@ class TestHelloWorld(unittest.TestCase):
         sys.stdout.assert_has_calls(
             [call.write('Hello world'), call.write('\n')],
             [call.write('Hello Kenji Morizono'), call.write('\n')]
+			[call.write('Hello Ryan McCullough'), call.write('\n')]
         )
