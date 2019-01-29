aaaafec309156ee3613cb7a325726c5f3ea8c83e
KenjiMorizono <kenji.morizono1@gmail.com>
2019-01-28 23:28:24 -0700

    Fixed issues with syntax in commit 90364bf

--- jvpm/HelloWorld.py
+++ jvpm/HelloWorld.py
@@ -2,7 +2,7 @@ def HelloWorld():
     print("Hello world")
     print("Hello Kenji Morizono")
     print("Hello Ryan McCullough")
-	print("Hello Ian Mutahi")
+    print("Hello Ian Mutahi")
 
 if '__main__' == __name__:
     HelloWorld()
