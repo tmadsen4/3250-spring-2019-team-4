69cec130fa3fa9798fc38c044979b5d6982e17e5
snubeagle <rmccull7@msudenver.edu>
2019-02-27 10:27:08 -0700

    fixed naming on tests and opcode methods

--- jvpm/op_codes1.py
+++ jvpm/op_codes1.py
@@ -28,24 +28,23 @@ class op_codes:
                 self.stack_z.append(5)
                 return 5
 
-
-	    def OP_code60():
+	    def op_code60():
                 var1 = data.pop() + data.pop()
                 stack_z.append(var1)
 
-	    def OP_code7e():
+	    def op_code7e():
 		        var1 = data.pop() & data.pop()
 		        stack_z.append(var1)
 
-	    def OP_code6c(self, data):
+	    def op_code6c(self, data):
 		        var1 = data.pop() / data.pop()
 		        stack_z.append(var1)
 
-	    def OP_code68(self, data):
+	    def op_code68(self, data):
 		        var1 = data.pop() * data.pop()
 		        stack_z.append(var1)
 
-	    def OP_codes74(self, data):
+	    def op_codes74(self, data):
 		        var1 = data.pop() * -1
 		        stack_z.append(var1)
 
--- jvpm/test/test_op_codes.py
+++ jvpm/test/test_op_codes.py
@@ -1,29 +1,28 @@
 import unittest
 from jvpm import op_codes1
-        
-'''
-   def test_OP_code60(self): 
-       	self.assert_(self, stack_z != 0)
-
-    def test_OP_code7e(self):
-    	self.assert_(self, stack_z != 0)
-
-    def test_OP_code6c(self):
-    	self.assert_(self, stack_z != 0)
-
-    def test_OP_code68(self):
-    	self.assert_(self, stack_z != 0)
-
-    def test_OP_code74(self):
-        self.assert_(self, stack_z != 0)
-
-    def test_push_to_stack(self):'''
 
 class test_op_codes(unittest.TestCase):
-    
+
+    test_stack = [1,2]
+
     def test_op_code70(self):# remainder
         self.assertEqual(op_codes1.op_codes.op_code70(11,2),1)
     def test_op_code78(self):# shift left
         self.assertEqual(op_codes1.op_codes.op_code78(1,2),4)
     def test_op_code80(self):# bitwise OR
         self.assertEqual(op_codes1.op_codes.op_code80(4,1),5)
+
+    def test_op_code60(self):
+        self.assert_(self, stack_z != 0)
+
+    def test_op_code7e(self):
+        self.assert_(self, stack_z != 0)
+
+    def test_op_code6c(self):
+        self.assert_(self, stack_z != 0)
+
+    def test_op_code68(self):
+        self.assert_(self, stack_z != 0)
+
+    def test_op_code74(self):
+        self.assert_(self, stack_z != 0)
