86646e0ea99a67fa4aab127c139335b219c2bed7
GeromanEmpire <47043651+GeromanEmpire@users.noreply.github.com>
2019-02-26 19:16:21 -0700

    Added Opcodes 05, 06, 07, and 08.

--- jvpm/op_codes1.py
+++ jvpm/op_codes1.py
@@ -5,12 +5,29 @@ All Methods for OP Codes go in this file
 class op_codes:
         def op_code70(x,y):     #remainder
                 return x%y
-        
-        def op_code78(x,y):     #shift left   
+
+        def op_code78(x,y):     #shift left
                 return x<<y
 
         def op_code80(x,y):     #bitwise OR
                 return x|y
+
+        def op_code05():        #loads 2 onto stack
+                self.stack_z.append(2)
+                return 2
+
+        def op_code06():        #loads 3 onto stack
+                self.stack_z.append(3)
+                return 3
+
+        def op_code07():        #loads 4 onto stack
+                self.stack_z.append(4)
+                return 4
+
+        def op_code08():        #loads 5 onto stack
+                self.stack_z.append(5)
+                return 5
+
 '''
 	def OP_code60(self, data):
                 var1 = data.pop() + data.pop()
