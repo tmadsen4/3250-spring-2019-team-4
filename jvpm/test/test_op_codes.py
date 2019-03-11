a8624b4579f34790becf69f71bb0f3643499189a
Agua <Agua@Aguas-MacBook-Pro.local>
2019-03-11 15:34:08 -0600

    updated shift right method in op_codes1

--- jvpm/test/test_op_codes.py
+++ jvpm/test/test_op_codes.py
@@ -37,7 +37,7 @@ class test_op_codes(unittest.TestCase):
     def test_op_code7c(self): #shift right
         test_stack = [1,2]
         test_stack = op_codes1.op_codes.op_code7c(test_stack)
-        self.assertEqual(test_stack.pop() , 5)
+        self.assertEqual(test_stack.pop() , 1)
 
     def test_op_code7a(self):  # arithmetic shift right
         test_stack = [2, 15]
